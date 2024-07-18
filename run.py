340601d841c08e7d52f30473adfac1092ebf711b
Declan <laresearchlabs@gmail.com>
2024-07-18 15:21:26 +0100

    add main site files

--- run.py
+++ run.py
@@ -28,6 +28,6 @@ def quiz():
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "127.0.0.1"),
-        port=int(os.environ.get("PORT", "5500")),  
+        port=int(os.environ.get("PORT", "8000")),  
         debug=True,
     )
